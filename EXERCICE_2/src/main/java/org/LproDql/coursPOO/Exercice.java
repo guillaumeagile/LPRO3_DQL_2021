8f447797a72b856cce6f6848bcda0a56f854d60b
g.saintetienne <g.saintetienne@primobox.com>
2021-12-01 15:21:02 +0100

    plus loin dans le refacto  et finalisation de la ré écriture

--- EXERCICE_2/src/main/java/org/LproDql/coursPOO/Exercice.java
+++ EXERCICE_2/src/main/java/org/LproDql/coursPOO/Exercice.java
@@ -1,6 +1,5 @@
 package org.LproDql.coursPOO;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,47 +8,35 @@ import java.util.stream.Collectors;
 
 public class Exercice {
 
+    public Map<Integer, String> compteLesNegatifsEtCOnstruitUneChaineAvecLesPositifs
+        (int nombreDeValeursAnalysees, List<Integer> valeursAAnalyser) {
 
-    public Map<Integer, String> compteLesNegatifsEtCOnstruitUneChaineAvecLesPositifs(int nombreDeValeursAnalysees, List<Integer> valeursAAnalyser) {
+        int nombreDEntierNegatifs = extraireLesNombresNegatifs(nombreDeValeursAnalysees, valeursAAnalyser).size();
 
-        HashMap<Integer, String> aRetourner = new HashMap<>();
-        int nombreDEntierNegatifs = 0;
         StringBuilder suiteDesEntiersPositifs = new StringBuilder();
-        for (int i = 0; i < valeursAAnalyser.size() && i < nombreDeValeursAnalysees; i++) {
-            Integer nombreExtrait = valeursAAnalyser.get(i);
-            if (nombreExtrait >= 0) {
-                suiteDesEntiersPositifs.append(String.format("%d ", nombreExtrait));
-            } else {
-                nombreDEntierNegatifs++;
-            }
+        String s = extraireLesNombresPositifs(nombreDeValeursAnalysees, valeursAAnalyser)
+            .stream().map(Object::toString).collect(Collectors.joining(" "));
+        suiteDesEntiersPositifs.append(s);
+
+
+        return ajouteUnRetourChariotEtPlanceLesElementsDansLaMap( nombreDEntierNegatifs, suiteDesEntiersPositifs);
         }
-        return ajouteUnRetourChariotEtPlanceLesElementsDansLaMap(aRetourner, nombreDEntierNegatifs, suiteDesEntiersPositifs);
-    }
 
-    public ArrayList<Integer> extraireLesNombresPositifs(int nombreDeValeursAnalysees, List<Integer> valeursAAnalyser) {
-
-        ArrayList<Integer> aRetourner = new ArrayList<Integer>();
-
-        for (int i = 0; i < valeursAAnalyser.size() && i < nombreDeValeursAnalysees; i++) {
-            Integer nombreExtrait = valeursAAnalyser.get(i);
-            aRetourner.add(nombreExtrait);
-        }
-        return aRetourner;
-    }
-
-    public List<Integer> extraireLesNombresPositifs_v2(int nombreDeValeursAnalysees, List<Integer> valeursAAnalyser) {
-
+        public List<Integer> extraireLesNombresPositifs(int nombreDeValeursAnalysees, List<Integer> valeursAAnalyser) {
         return valeursAAnalyser.stream().limit(nombreDeValeursAnalysees).filter(x -> x >= 0).collect(Collectors.toList());
 
     }
 
     private HashMap<Integer, String> ajouteUnRetourChariotEtPlanceLesElementsDansLaMap(
-        HashMap<Integer, String> aRetourner,
         int nombreDEntierNegatifs,
         StringBuilder suiteDesEntiersPositifs) {
+        HashMap<Integer, String> aRetourner = new HashMap<>();
         suiteDesEntiersPositifs.append("\n");
-        HashMap<Integer, String> nouvelleHashMap = (HashMap<Integer, String>) aRetourner.clone();
-        nouvelleHashMap.put(nombreDEntierNegatifs, suiteDesEntiersPositifs.toString());
-        return nouvelleHashMap;
+        aRetourner.put(nombreDEntierNegatifs, suiteDesEntiersPositifs.toString());
+        return aRetourner;
+    }
+
+    public List<Integer> extraireLesNombresNegatifs(int nombreDeValeursAnalysees, List<Integer> valeursAAnalyser) {
+        return valeursAAnalyser.stream().limit(nombreDeValeursAnalysees).filter(x -> x < 0).collect(Collectors.toList());
     }
 }
--- EXERCICE_2/src/test/java/org/LproDql/coursPOO/tests/ExerciceTests.java
+++ EXERCICE_2/src/test/java/org/LproDql/coursPOO/tests/ExerciceTests.java
@@ -57,7 +57,7 @@ public class ExerciceTests {
         Entry<Integer, String> firstResult = actual.entrySet().iterator().next();
         Assertions.assertThat(firstResult.getKey()).isZero();
         //2e verification:  que contient la chaine de caractère?
-        Assertions.assertThat(firstResult.getValue()).isEqualTo("8 \n");
+        Assertions.assertThat(firstResult.getValue()).isEqualTo("8\n");
     }
 
    // @Test
@@ -88,7 +88,7 @@ public class ExerciceTests {
         //then / ASSERT
         Entry<Integer, String> firstResult = actual.entrySet().iterator().next();
         Assertions.assertThat(firstResult.getKey()).isEqualTo(1);
-        Assertions.assertThat(firstResult.getValue()).isEqualTo("9 \n");
+        Assertions.assertThat(firstResult.getValue()).isEqualTo("9\n");
     }
 
     @Test
@@ -107,7 +107,7 @@ public class ExerciceTests {
         //then / ASSERT
         Entry<Integer, String> firstResult = actual.entrySet().iterator().next();
         Assertions.assertThat(firstResult.getKey()).isEqualTo(1);
-        Assertions.assertThat(firstResult.getValue()).isEqualTo("4 9 8 \n");
+        Assertions.assertThat(firstResult.getValue()).isEqualTo("4 9 8\n");
     }
 
     @Test
@@ -124,7 +124,7 @@ public class ExerciceTests {
         //then / ASSERT
         Entry<Integer, String> firstResult = actual.entrySet().iterator().next();
         Assertions.assertThat(firstResult.getKey()).isZero();
-        Assertions.assertThat(firstResult.getValue()).isEqualTo("1 9 \n");
+        Assertions.assertThat(firstResult.getValue()).isEqualTo("1 9\n");
     }
 
     @Test
@@ -137,7 +137,7 @@ public class ExerciceTests {
         listeEntiers.add(8);
 
         //when / ACT
-        List<Integer> actual = exercice.extraireLesNombresPositifs_v2(3, listeEntiers);
+        List<Integer> actual = exercice.extraireLesNombresPositifs(3, listeEntiers);
         //then / ASSERT
         Assertions.assertThat(actual).isEqualTo(Arrays.asList(1, 9, 8)  );
     }
@@ -152,9 +152,24 @@ public class ExerciceTests {
         listeEntiers.add(8);
 
         //when / ACT
-        List<Integer> actual = exercice.extraireLesNombresPositifs_v2(2, listeEntiers);
+        List<Integer> actual = exercice.extraireLesNombresPositifs(2, listeEntiers);
         //then / ASSERT
         Assertions.assertThat(actual).isEqualTo(Arrays.asList(1, 9)  );
+    }
+
+    @Test
+    public void listeAvecTroisNegatifsLimiteA2_avecNouvelleFonction() {  // règle des 3 A
+        //given  / ARRANGE
+        final Exercice exercice = new Exercice();
+        final List<Integer> listeEntiers = new ArrayList<>();
+        listeEntiers.add(-1);
+        listeEntiers.add(-9);
+        listeEntiers.add(-8);
+
+        //when / ACT
+        List<Integer> actual = exercice.extraireLesNombresNegatifs(2, listeEntiers);
+        //then / ASSERT
+        Assertions.assertThat(actual).isEqualTo(Arrays.asList(-1, -9)  );
     }
 
 }
